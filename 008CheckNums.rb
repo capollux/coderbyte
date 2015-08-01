5256f6d46f366bbab418273d8f53ee5d4bb46e0e
Hwang Junwoo <capollux10@gmail.com>
2015-08-02 01:40:22 +0900

    Update 008CheckNums.rb

--- 008CheckNums.rb
+++ 008CheckNums.rb
@@ -1,12 +1,7 @@
 num1 = 3 #Can Change
 num2 = 122 #Can Change
 
-flag="true"
-
-if num1==num2
-	flag="-1"
-elsif num1>num2 
-	flag="false"
-end
+flag=(num1>num2)
+flag="-1" if num1==num2
 
 puts flag
